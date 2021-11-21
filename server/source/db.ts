86e95580d876ef96f7cb9178b1a72f68e2881753
Joshua Challenger <jchallenger1@hotmail.com>
2021-11-20 19:34:20 -0500

    migrated vanilla js server/db to typescript

--- server/source/db.ts
+++ server/source/db.ts
@@ -1,61 +1,58 @@
-import Pool from 'pg'
+import pg from 'pg'
 import fs from 'fs'
 
 const dbName = "bookstore"
-
+const SQLFolder = '../SQL'
 /* Credentials to the local postgresql database */
-function getPool() {
-    return new Pool.Pool({
-        user: "postgres",
-        password: "2",
-        host: "localhost",
-        port: 5432,
-        database: dbName
-    });
-}
+const pool = new pg.Pool({
+    user: "postgres",
+    password: "2",
+    host: "localhost",
+    port: 5432,
+    database: dbName
+});
 
-export class Database {
-    dbName = "bookstore"
-    pool: Pool.Pool;
-
-    constructor() {
-        this.pool = getPool();
-        this.initalizeDB();
+/* Creates tables in the bookstore database and populates them if needed*/
+fs.readFile(`${SQLFolder}/DDL.sql`, "utf-8", async function(err, data) {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
     }
 
-    /**
-     * initalizeDB
-     */
-    public initalizeDB() {
-        /* Creates tables in the bookstore database and populates them if needed*/
-        fs.readFile('../SQL/DDL.sql', "utf-8", async function(err, data) {
+    let populateDB = () => {
+        /* populates the db if empty */
+        fs.readFile(`${SQLFolder}/populate.sql`, "utf-8", async function(err, pop_data) {
             if (err) {
                 console.error(err);
                 process.exit(-1);
             }
+            const authors = await pool.query("SELECT * FROM author LIMIT 1");
+        
+            if (authors.rowCount == 1) return;
 
-            let populateDB = () => {
-                /* populates the db if empty */
-                fs.readFile('../SQL/populate.sql', "utf-8", async function(err, pop_data) {
-                    if (err) {
-                        console.error(err);
-                        process.exit(-1);
-                    }
-                    const authors = await this.pool.query("SELECT * FROM author LIMIT 1");
-                
-                    if (authors.rowCount == 1) return;
-
-                    this.pool.query(pop_data)
-                        .then(() => console.log(`Added ${pop_data.split(/\r\n|\r|\n/).length} tuples`))
-                        .catch(err => console.log(err.stack));
-                    
-                });
-            }
-
-            this.pool.query(data)
-                .then(() => console.log("Successfully created database tables"))
-                .then(populateDB)
+            pool.query(pop_data)
+                .then(() => console.log(`Added ${pop_data.split(/\r\n|\r|\n/).length} tuples`))
                 .catch(err => console.log(err.stack));
-        })
+            
+        });
     }
+
+    pool.query(data)
+        .then(() => console.log("Successfully created database tables"))
+        .then(populateDB)
+        .catch(err => console.log(err.stack));
+});
+
+class Database {
+    pool: pg.Pool;
+
+    constructor(pool: pg.Pool) {
+        this.pool = pool;
+    }
+}
+
+const db = new Database(pool);
+
+export function getDB() {
+    return db;
 }
--- server/source/server.ts
+++ server/source/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import {Database} from './db'
+import {getDB} from './db'
 
 const port = 5000;
 const app = express();
@@ -11,8 +11,13 @@ app.use(express.json());
 
 
 app.get('/', async(req, res) => {
-    //const authors = await pool.query("SELECT * FROM author");
-    //res.json(authors.rows);
+    let db = getDB();
+    try {
+        const authors = await db.pool.query("SELECT * FROM author");
+        res.json(authors.rows);
+    } catch (error:any) {
+        console.error(error.message)
+    }
 });
 
 app.listen(port, () => {

ac2970c58c77fb0f91f0f0c9b170779184709098
Joshua Challenger <jchallenger1@hotmail.com>
2021-11-24 16:57:14 -0500

    added runTransaction function for running multiple queries with ACID consistency

--- server/source/db.ts
+++ server/source/db.ts
@@ -52,6 +52,15 @@ export function makeResponse(queryResult:string[] | pg.QueryResult<any>) {
         "rows": queryResult
     }
 }
+
+// Interface for a transaction
+// provide EITHER rawQuery OR predefinedQuery, do not provide both.
+export interface Transaction {
+    rawQuery?:string,
+    predefinedQuery?:string,
+    parameters:[...any]
+}
+
 class Database {
     pool: pg.Pool;
     sqlQueries: Map<string, string>;
@@ -65,7 +74,6 @@ class Database {
 
     public async runPredefinedQuery(queryName: string, parameters: [...any]) {
         if (!this.sqlQueries.has(queryName)) {
-            console.log(this.sqlQueries);
             throw new Error(`Query "${queryName}" does not exist!`);
         }
         try {
@@ -75,6 +83,59 @@ class Database {
             console.log(error);
             return makeResponse([])
         }
+    }
+
+    /**
+     * Function runs multiple queries (transactions) in one go, this function provides database
+     * consistency as any error defined in transaction will result in a rollback of the database
+     * prior to any transaction
+     * Function can throw exceptions
+     * @param transactions The queries to run
+     * @returns A one to one mapping of transactions where each element is the result of said query
+     */
+    public async runTransaction(transactions:Transaction[]): Promise<[...any]> {
+        const client = await pool.connect();
+
+        let promises = []
+
+        try {
+            // start a transaction history
+            await client.query('BEGIN');
+
+            // for each transaction, map it to a promise to furfill the query
+            promises = transactions.map(async transaction => {
+                // raw query, just apply it directly
+                if (transaction.rawQuery) {
+                    return makeResponse(await client.query(transaction.rawQuery, transaction.parameters));
+                }
+                // query is defined in /SQL/Queries, run this predefined query if it exists
+                if (transaction.predefinedQuery) {
+
+                    if (!this.sqlQueries.has(transaction.predefinedQuery))
+                        throw new Error(`Query "${transaction.predefinedQuery}" does not exist!`);
+                    return makeResponse(await client.query(this.sqlQueries.get(transaction.predefinedQuery)!, transaction.parameters));
+
+                }
+                throw new Error("Transaction has no query to compute");
+            });
+
+            await client.query('COMMIT')
+        } 
+        catch(e) {
+            await client.query('ROLLBACK')
+            throw e
+        }
+        finally {
+            client.release()
+        }
+
+        // Decompose all the promises and map them to actual data
+        let results:[...any] = []
+        await Promise.all(promises).then( (promiseResult) => {
+            results.push(promiseResult);
+        })
+        
+        return results;
     }
 
 }

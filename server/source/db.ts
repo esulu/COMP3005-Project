bd2c35348f2e9d119e7c0c069e471e13c75fd336
Joshua Challenger <jchallenger1@hotmail.com>
2021-11-24 20:00:21 -0500

    added comments and changed code to actually catch errors and prevent promise failures, but now it fails on an insert?

--- server/source/db.ts
+++ server/source/db.ts
@@ -1,6 +1,7 @@
 import pg from 'pg'
 import fs from 'fs'
 import internal from 'stream';
+import e from 'express';
 
 const dbName = "bookstore"
 const SQLFolder = '../SQL/Queries'
@@ -80,6 +81,14 @@ class Database {
         this.sqlQueries = preDefinedQueries;
     }
 
+    /**
+     *  Function runs a predefined query file under /SQL/Queries/queryName and returns the result of the query
+     * @param queryName The name of the query under /SQL/Queries/{queryName}
+     * @param parameters Any paramters required to be set in the query file
+     * @param client Optionaly, the client database (pool.client) to ensure a single transaction occurs to the database
+     * Only provide this if this function is being used under runTransaction()
+     * @returns 
+     */
     public async runPredefinedQuery(queryName: string, parameters: [...any], client:undefined|pg.PoolClient = undefined) {
         if (!this.sqlQueries.has(queryName)) {
             throw new Error(`Query "${queryName}" does not exist!`);
@@ -107,14 +116,16 @@ class Database {
     public async runTransactions(transactions:Transaction[], client:undefined|pg.PoolClient = undefined): Promise<[...any]> {
         if (client === undefined)
             client = await pool.connect();
-        let promises = []
 
+        let results:[...any] = []
         try {
             // start a transaction history
             await client.query('BEGIN');
 
+            let errorMessage = undefined;
             // for each transaction, map it to a promise to furfill the query
-            promises = transactions.map(async transaction => {
+            // Promise.all decomposes all the promises and map them to actual data
+            await Promise.all(transactions.map(async transaction => {
                 // raw query, just apply it directly
                 if (transaction.rawQuery) {
                     return makeResponse(await client!.query(transaction.rawQuery, transaction.parameters));
@@ -125,52 +136,63 @@ class Database {
                     if (!this.sqlQueries.has(transaction.predefinedQuery))
                         throw new Error(`Query "${transaction.predefinedQuery}" does not exist!`);
                     return makeResponse(await client!.query(this.sqlQueries.get(transaction.predefinedQuery)!, transaction.parameters));
-
                 }
                 throw new Error("Transaction has no query to compute");
+            })).then( promiseResult => {
+                results.push(promiseResult);
+            }).catch( e => {
+                errorMessage = e; // we can't throw inside a .catch, we have to move it outside first
             });
+            if (errorMessage) throw errorMessage;
 
             await client.query('COMMIT')
         } 
         catch(e) {
             await client.query('ROLLBACK')
-            throw e
+            throw e;
         }
         finally {
             client.release()
         }
 
-        // Decompose all the promises and map them to actual data
-        let results:[...any] = []
-        await Promise.all(promises).then( (promiseResult) => {
-            results.push(promiseResult);
-        })
-        
         return results;
     }
 
-    public async runTransaction(queryCreator: (client:pg.PoolClient) => Promise<boolean>) {
+    /**
+     * Function runs a query defined by query creator in a single transaction. This is done by providing the COMMIT and ROLLBACK
+     * functionality to ensure a singular transaction while the querycreator performs the actual queries
+     * @param queryCreator A function accepting the client of which is a single connection to the database
+     *  pass the client variable to any other function in Database to ensure a single connection
+     *  IMPORTANT: The function must be wrapped in a try catch block and returns the following:
+     *      boolean -> indication of whether it had any errors
+     *      string -> the error message
+     */
+    public async runTransaction(queryCreator: (client:pg.PoolClient) => Promise<[boolean, string]>) {
         let client = await pool.connect();
 
         try {
             // start a transaction history
             await client.query('BEGIN');
 
-            queryCreator(client).then(async doRollBack => {
-                if (doRollBack) {
-                    console.log("rolling back...");
-                    await client.query("ROLLBACK")
+            // in the case of an error, keep the message outside of the promise functions
+            let errorMessage = undefined;
+
+            // run the query the user wants to perform in a single transaction
+            await queryCreator(client).then(async doRollBack => {
+                if (doRollBack[0]) {
+                    errorMessage = doRollBack[1];
                 } else {
-                    console.log("committing...");
                     await client.query('COMMIT');
                 }
             }).catch( async e => {
-                console.log("rolling back...");
-                await client.query("ROLLBACK")
+                errorMessage = e.message;
             })
+
+            // If at any point we had an error, throw said error, this functionality cannot be in .then or .catch
+            if (errorMessage)
+                throw errorMessage;
         } 
         catch(e) {
-            console.log("rolling back the db...");
             await client.query('ROLLBACK')
             throw e
         }
--- server/source/server.ts
+++ server/source/server.ts
@@ -217,9 +217,9 @@ app.use('/verifyUser', (req, res) => {
     db.runPredefinedQuery("verifyUser", [req.body.token, req.body.password])
         .then(query_result => {
             if (query_result["rowCount"] == 0)
-                res.json({status:"400", text:"Either the provided token doesn't exist or the password is incorrect"});
+                res.json({status:400, text:"Either the provided token doesn't exist or the password is incorrect"});
             else
-                res.json({ status:"200", text:"" });
+                res.json({ status:200, text:"" });
         })
         .catch(error => {
             console.log(error);
@@ -229,11 +229,11 @@ app.use('/verifyUser', (req, res) => {
 app.use('/checkout', async (req, res) => {
     let user_id = getIntParameter(req.body.token, -1, -1, 10000000);
 
-    async function doCheckout(client:pg.PoolClient) {
+    async function doCheckout(client:pg.PoolClient) : Promise<[boolean, string]>{
+        // Whenever we run a query in our transaction, it shouldn't return an empty table
         let insureIntegrity = (query:QueryResult) => {
-            console.log(query);
             if (query.rowCount === 0)
-                throw "A query had no rows returned!";
+                throw "Empty data was returned, this is most likely due to the user not having anything in their cart.";
         }
         try {
             // Get the warehouse key
@@ -242,26 +242,19 @@ app.use('/checkout', async (req, res) => {
             let warehouse_id = warehouses.rows[0].warehouse_id;
 
             // Get the cart key
-            console.log("warehouse_id = " +warehouse_id);
-            console.log("user_id = " +user_id);
             let carts = await db.runPredefinedQuery("userCarts", [user_id], client);
             insureIntegrity(carts);
             let cart_id = carts.rows[0].cart_id;
-            console.log("cart_id = " + cart_id);
 
             // Create a shipping tuple and get the key
             let insertShipping = await db.runPredefinedQuery("insertShipping", [_.sample(db.shippingCompanies), db.shippingStatuses[0],warehouse_id]);
             insureIntegrity(insertShipping);
             let shipping_id = insertShipping.rows[0].shipping_id;
-            console.log("shipping_id = " + shipping_id);
-            
             // Create a new cart
             let insertCart = await db.runPredefinedQuery("insertCart", [], client);
             insureIntegrity(insertCart);
-            let new_cart_id = insertCart.rows[0].cart_id
-            console.log("new cart id="+new_cart_id);
-            console.log([req.body.address, req.body.bankNumber, shipping_id, user_id, cart_id]);
-
+            let new_cart_id = insertCart.rows[0].cart_id;
+            console.log(new_cart_id);
             (await db.runTransactions([{
                 // Insert the new order tuple
                 predefinedQuery: "insertOrder",
@@ -271,22 +264,19 @@ app.use('/checkout', async (req, res) => {
                 predefinedQuery: "setUserCart",
                 parameters: [new_cart_id, user_id]
             }])).forEach( t => insureIntegrity(t));
-
-            throw "";
-        } catch(error) {
-            return true;
+        } catch(error:any) {
+            return [true, error];
         }
-        return false;
+        return [false, ""];
     }
 
     db.runTransaction(doCheckout)
-        .then( res=> {
-            console.log("result "+res);
+        .then( () => {
+            res.json({ status:200})
         }).catch( err => {
             console.log(err);
+            res.json({status:400, error:err})
         });
-
-
 });
 
 app.listen(port, () => {

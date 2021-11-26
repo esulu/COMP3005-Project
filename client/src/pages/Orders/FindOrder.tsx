8cbc2788c2f818c8441fd84f1b25a31cb257fd53
Joshua Challenger <jchallenger1@hotmail.com>
2021-11-26 13:06:50 -0500

    added layout for find order component

---
+++ client/src/pages/Orders/FindOrder.tsx
@@ -0,0 +1,9 @@
+
+
+const FindOrder = () => {
+
+    return (
+        <>
+        </>
+    )    
+}
--- client/src/pages/Orders/Orders.tsx
+++ client/src/pages/Orders/Orders.tsx
@@ -1,10 +1,23 @@
 
 import Box from '@mui/material/Box';
 import { OrderTable } from "./OrderTable";
+import { Typography } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const Orders = () => {
     return (
-        <Box mt={2}>
+        <Box m={2} >
+            <Stack direction="row" justifyContent="center" alignItems="center" spacing={5}>
+                <Typography variant="h6">
+                    Looking for an order based on Tracking Number?
+                </Typography>
+                <Button variant="outlined" startIcon={<SearchIcon />}>
+                    Find Order
+                </Button>
+            </Stack>
+            <span style={{margin:5}}></span>
             <OrderTable />
         </Box>
     );

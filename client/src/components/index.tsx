7217346b698d3d2b4f3b10dbf9c31b6bc36418e6
Eren Sulutas <erensulu11@gmail.com>
2021-11-26 19:13:04 -0500

    added query to add into written_by. also added a new admin user and added increment to author ID

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,6 +1,4 @@
 /* Add component exports to this file */
-export * from './SampleInput/Input';
-export * from './SampleList/List';
 export * from './Navigation/NavBar';
 export * from './Book/Book';
 export * from './Book/BookStore';
